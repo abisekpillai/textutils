41b00c74254ba768082588a41e96ef1e69b00410
Abishek R <abisekpillai@gmail.com>
2023-06-24 03:09:20 +0530

    exploring Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import TestForm from './components/TestForm';
+import About from './components/About';
 
 let name = "Abisek";
 
@@ -8,6 +10,10 @@ function App() {
   return (
     <>
     <Navbar title="TextUtils"/>
+    <div className="container my-3">
+      <TestForm heading="Enter the text to be analyzed"/>
+      <About/>
+    </div>
     </>
     );
 }
---
+++ src/components/TestForm.js
@@ -0,0 +1,57 @@
+import React, {useState} from 'react'
+
+export default function TextForm(props) {
+    const [text, setText] = useState("Enter text here");
+
+    const handleUpClick = ()=>{
+        console.log("Uppercase was clicked");
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+
+    const handleLowClick = ()=> {
+        console.log("Lowercase was clicked");
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+
+    const handleClearClick = ()=> {
+        console.log("Clear Text was clicked");
+        setText("");
+    }
+
+    const handleCopyClick = ()=> {
+        console.log("Copy Text was clicked");
+        var text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+
+    const handleOnChange = (event)=>{
+        console.log("OnChange");
+        setText(event.target.value);
+    }
+
+    return (
+    <>
+    <div className="container">
+        <h2>{props.heading}</h2>
+        <div className="mb-3">
+            <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+        </div>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to Lowercase</button>
+        <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
+        <button className="btn btn-primary mx-1" onClick={handleCopyClick}>Copy Text</button>
+    </div>
+    <div className="container my-3">
+        <h2>Your text summary</h2>
+        <p>{text.split(".").length} sentences, {text.split(" ").length} words and {text.length} characters</p>
+
+        <h3>Preview</h3>
+        <p>{text}</p>
+    </div>
+    </>
+  )
+}
+ 
