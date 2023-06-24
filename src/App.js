344142861a8e52d881b74164e70f4305d3b017f2
Abishek R <abisekpillai@gmail.com>
2023-06-24 18:07:30 +0530

    improvised Darkmode

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TestForm from './components/TestForm';
-import About from './components/About';
-
-let name = "Abisek";
+import { useState } from 'react';
 
 function App() {
+  
+  const [mode,setMode] = useState('light');
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#393053';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
+
   return (
     <>
-    <Navbar title="TextUtils"/>
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
     <div className="container my-3">
-      <TestForm heading="Enter the text to be analyzed"/>
-      <About/>
+      <TestForm heading="Enter the text to be analyzed" mode={mode} toggleMode={toggleMode}/>
     </div>
     </>
     );
--- src/components/TestForm.js
+++ src/components/TestForm.js
@@ -1,9 +1,9 @@
 import React, {useState} from 'react'
 
 export default function TextForm(props) {
-    const [text, setText] = useState("Enter text here");
+    const [text, setText] = useState('');
 
-    const handleUpClick = ()=>{
+    const handleUpClick = ()=>{ 
         console.log("Uppercase was clicked");
         let newText = text.toUpperCase();
         setText(newText);
@@ -34,22 +34,22 @@ export default function TextForm(props) {
 
     return (
     <>
-    <div className="container">
+    <div className="container" style={{color: props.mode==='light'?'black':'white'}}>
         <h2>{props.heading}</h2>
         <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='light'?'white':'#443C68',color: props.mode==='light'?'black':'white'}} id="myBox" rows="8"></textarea>
         </div>
         <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
         <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to Lowercase</button>
         <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
         <button className="btn btn-primary mx-1" onClick={handleCopyClick}>Copy Text</button>
     </div>
-    <div className="container my-3">
-        <h2>Your text summary</h2>
+    <div className="container my-3" style={{color: props.mode==='light'?'black':'white'}}>
+        <h2 style={{}}>Your text summary</h2>
         <p>{text.split(".").length} sentences, {text.split(" ").length} words and {text.length} characters</p>
 
         <h3>Preview</h3>
-        <p>{text}</p>
+        <p>{text.length>0?text:'Enter something to preview it here'}</p>
     </div>
     </>
   )
